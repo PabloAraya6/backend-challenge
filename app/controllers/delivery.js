2a39f31fc6bdfdf0ba49f7170dedc990c09aa311
Pablo <arayap812@gmail.com>
2021-08-01 16:17:17 -0300

    change info data

--- app/controllers/delivery.js
+++ app/controllers/delivery.js
@@ -3,8 +3,8 @@ const faker = require('faker')
 
 const delivery = async (req, res, next) => {
     try {
-        var distance = parseInt(faker.datatype.number({min:0, max:2000}))
-        var num = parseInt(Math.abs(distance/100))
+        var distance = parseInt(faker.datatype.number({ min: 0, max: 2000 }))
+        var num = parseInt(Math.abs(distance / 100))
 
         var a = 0, b = 1, aux
         while (num > 0) {
@@ -13,16 +13,21 @@ const delivery = async (req, res, next) => {
             b = aux
             num--
         }
+
+        let data
+        if (a == 0) {
+            data = `Delivery time is today`
+        } else {
+            data = `Delivery time is ${a} day(s)`
+        }
+
         res.status(200).json({
-            message:'success',
-            info: [
-                `Random distance [0km - 2000km]: ${distance} km`,
-                ``
-            ],
-            data: `Delivery time is ${a} days`
+            message: 'success',
+            info: `Random distance [0km - 2000km]: ${distance} km`,
+            data
         })
     } catch (error) {
-        next(error)        
+        next(error)
     }
 }
 
