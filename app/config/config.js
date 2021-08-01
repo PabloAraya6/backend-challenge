98813bcd80674ecc43a154b72b4f3beae415aad5
Pablo <arayap812@gmail.com>
2021-07-31 21:38:02 -0300

    add API_KEY in env

--- app/config/config.js
+++ app/config/config.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 
-const config = {
-    DATABASE_HOST: 'localhost' || procces.env.DATABASE_HOST,
-    DATABASE_USER: 'root' || procces.env.DATABASE_USER,
-    DATABASE_PASSWORD: 'admin' || procces.env.DATABASE_PASSWORD,
-    DATABASE_NAME: 'enviame' || procces.env.DATABASE_NAME,
+const env = {
+    API_KEY: 'ea670047974b650bbcba5dd759baf1ed' || process.env.API_KEY,
+    DATABASE_HOST: 'localhost' || process.env.DATABASE_HOST,
+    DATABASE_NAME: 'enviame' || process.env.DATABASE_NAME,
+    DATABASE_USER: 'root' || process.env.DATABASE_USER,
+    DATABASE_PASSWORD: 'admin' || process.env.DATABASE_PASSWORD,
     PORT: '5001' || procces.env.PORT,
 }
 
-module.exports = config
+module.exports = env
